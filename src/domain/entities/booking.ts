494db490614d775b9184a116580cd16a8990583a
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2025-03-06 21:42:48 -0300

    feat: :sparkles: Adiciona status "CONFIRMED" à entidade Booking e validação de reservas sobrepostas

--- src/domain/entities/booking.ts
+++ src/domain/entities/booking.ts
@@ -3,23 +3,33 @@ import { Property } from "./property";
 import { User } from "./user";
 
 export class Booking {
-  private readonly id: string;
-  private readonly property: Property;
-  private readonly user: User;
-  private readonly dateRange: DateRange;
-  private readonly numberOfGuests: number;
   constructor(
-    id: string,
-    property: Property,
-    user: User,
-    dateRange: DateRange,
-    numberOfGuests: number
+    private readonly id: string,
+    private readonly property: Property,
+    private readonly user: User,
+    private readonly dateRange: DateRange,
+    private readonly numberOfGuests: number,
+    private readonly status: "CONFIRMED" | "CANCELLED" = "CONFIRMED"
   ) {
+    this.validateBooking(property, dateRange);
     this.id = id;
     this.property = property;
     this.user = user;
     this.dateRange = dateRange;
     this.numberOfGuests = numberOfGuests;
+
+    property.addBooking(this);
+  }
+  private validateBooking(property: Property, dateRange: DateRange): void {
+    const bookings = property.getBookings();
+    const booking = bookings.find((booking) =>
+      booking.getDateRange().overlaps(dateRange)
+    );
+    if (booking && booking.getStatus() === "CONFIRMED") {
+      throw new Error(
+        "Imóvel indisponível. Já existe uma reserva confirmada para este imóvel neste período"
+      );
+    }
   }
 
   getId(): string {
@@ -40,5 +50,9 @@ export class Booking {
 
   getNumberOfGuests(): number {
     return this.numberOfGuests;
+  }
+
+  getStatus(): "CONFIRMED" | "CANCELLED" {
+    return this.status;
   }
 }
